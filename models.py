eb99565ed0744a66c1ca1dd6d7ecc936e85ebd42
Ari Pakman <aripakman@gmail.com>
2021-04-16 14:34:22 +0300

    small fix for high d

--- models.py
+++ models.py
@@ -95,7 +95,7 @@ class CDFNet(nn.Module):
                        self.nonneg_m(sliced_ws[-1]), utils.sigmoiddot(phis))
     
     
-    fm = -t.log(phidots).detach()    
+    fm = -t.log(phidots+1e-10).detach()    
     fm = fm.mean(3,True)
     fm = fm.min(2,True)[0]
     
@@ -145,8 +145,9 @@ class CDFNet(nn.Module):
     # inds : list of indices to restrict to (if interested in a subset of variables)
 
     phidots, fm = self.phidots(X, inds)
-    a = self.nonneg(self.a_s[-1])
-    f = t.einsum('mi,i->m', phidots, a / a.sum())
+    # a = self.nonneg(self.a_s[-1])
+    # f = t.einsum('mi,i->m', phidots, a / a.sum())
+    f = phidots.mean(1)
     return f, fm
 
 
