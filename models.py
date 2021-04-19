fixed normalization for marginals

models.py
@@ -141,7 +141,11 @@ class CDFNet(nn.Module):
 
   def log_density(self, X, inds=...):
     lk, fm = self.likelihood(X, inds)
-    return t.log(lk + 1e-10) - fm * self.d
+    if inds == ...:
+      n_vars = self.d
+    else:
+      n_vars = len(inds)
+    return t.log(lk + 1e-10) - fm * n_vars
 
   def nll(self, X, inds=...):
     # negative log likelihood

utils.py
@@ -2,7 +2,7 @@ import torch as t
 import numpy as np
 from scipy.stats import norm
 from torch.distributions.normal import Normal
-from copulae import GumbelCopula
+#from copulae import GumbelCopula
 from torch.utils.data import TensorDataset, DataLoader
 import argparse
 import os