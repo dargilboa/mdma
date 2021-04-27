bbc5e969b8ec446a6353eb3f400b3e3fc42ba384
tvatter <thibault.vatter@gmail.com>
2021-04-27 18:40:25 +0000

    clean

--- cdfnet/models.py
+++ cdfnet/models.py
@@ -4,7 +4,6 @@ import numpy as np
 from cdfnet import utils
 from operator import itemgetter
 from tltorch import TRL
-from tltorch.functional.tensor_regression import trl, tucker_trl
 import tensorly as tl
 tl.set_backend('pytorch')
 from tensorly import tenalg
@@ -72,27 +71,6 @@ class TenNet(nn.Module):
     # import pdb
     # pdb.set_trace()
 
-  def phis(self, Xn, inds=...):
-    # Xn is a M x 1 x n x dim(inds) tensor
-    # returns M x n x dim(inds) tensor
-    phis = Xn  # cleaner to move expand_X inside and take M x dim(inds) as input, like phidots
-
-    # keep only the parameters relevant for the subset of variables specified by inds
-    sliced_ws = [w[inds, ...] for w in self.w_s]
-    sliced_bs = [b[inds, ...] for b in self.b_s]
-    sliced_as = [a[inds, ...] for a in self.a_s]
-
-    # compute CDF using a feed-forward network
-    for w, b, a in zip(sliced_ws[:-1], sliced_bs[:-1], sliced_as[:-1]):
-      phis = einsum('mjik,jikl->mjil', phis, self.nonneg_m(w)) + b
-      phis = phis + t.tanh(phis) * t.tanh(a)
-
-    phis = einsum('mjik,jikl->mjil', phis, self.nonneg_m(
-        sliced_ws[-1])) + sliced_bs[-1]
-
-    phis = self.phi(phis)
-    return t.squeeze(phis, -1)
-
   def phidots(self, X, inds=...):
     # X is a M x dim(inds) tensor
     # returns M x n tensor
@@ -137,18 +115,6 @@ class TenNet(nn.Module):
     Xn = X.expand(-1, -1, self.n, 1)  # M x d x n x 1
     return Xn
 
-  def CDF(self, X, inds=...):
-    # Evaluate joint CDF at X
-    # X : M x len(inds) tensor of sample points
-    # returns M dim tensor
-
-    Xn = self.expand_X(X)
-    phis = self.phis(Xn, inds)
-    phis = t.prod(phis, 1)
-    a = self.nonneg(self.a_s[-1])
-    F = einsum('mi,i->m', phis, a / a.sum())
-    return F
-
   def likelihood(self, X, inds=...):
     # Evaluate joint likelihood at X
     # X : M x d tensor of sample points
@@ -169,16 +135,6 @@ class TenNet(nn.Module):
 
     return lk, fm
 
-  def marginal_likelihood(self, X):
-    marg_l = t.prod(t.stack(
-        [self.likelihood(X[:, i], inds=[i]) for i in range(self.d)]),
-                    dim=0)
-    return marg_l
-
-  def marginal_nll(self, X):
-    log_marginal_density = t.log(self.marginal_likelihood(X))
-    return -t.mean(log_marginal_density)
-
   def log_density(self, X, inds=...):
     lk, fm = self.likelihood(X, inds)
     if inds is ...:
@@ -191,169 +147,6 @@ class TenNet(nn.Module):
     # negative log likelihood
     # X : M x d tensor of sample points
     return -t.mean(self.log_density(X, inds=inds))
-
-
-# class TenNet(nn.Module):
-#   def __init__(
-#       self,
-#       d,
-#       n=100,
-#       L=4,
-#       m=5,
-#       w_std=0.1,
-#       b_bias=0,
-#       b_std=0,
-#       a_std=1.0,
-#       HT_poolsize=2,
-#       use_HT=False,
-#   ):
-#     super(TenNet, self).__init__()
-#     self.d = d
-#     self.n = n
-#     self.m = m
-#     self.L = L
-#     self.phi = t.sigmoid
-#     self.phidot = lambda x: t.sigmoid(x) * (1 - t.sigmoid(x))
-#     self.nonneg = t.nn.Softplus(10)
-#     self.nonneg_m = t.nn.Softplus(10)  # try beta=1?
-#     self.w_std = w_std
-#     self.a_std = a_std
-#     self.b_std = b_std
-#     self.b_bias = b_bias
-#     self.use_HT = use_HT
-
-#     # initialize parameters for marginal CDFs
-#     assert self.L >= 2
-#     w_scale = self.w_std / t.sqrt(t.Tensor([self.m]))
-#     self.w_s = t.nn.ParameterList(
-#         [nn.Parameter(self.w_std * t.randn(self.n, self.m, 1))])
-#     self.b_s = t.nn.ParameterList(
-#         [nn.Parameter(self.b_std * t.randn(self.n, self.m))])
-#     self.a_s = t.nn.ParameterList(
-#         [nn.Parameter(self.a_std * t.randn(self.n, self.m))])
-#     for i in range(self.L - 1):
-#       self.w_s += [nn.Parameter(w_scale * t.randn(self.n, self.m, self.m))]
-#       self.b_s += [nn.Parameter(self.b_std * t.randn(self.n, self.m))]
-#       self.a_s += [nn.Parameter(self.a_std * t.randn(self.n, self.m))]
-
-#     self.w_s += [nn.Parameter(w_scale * t.randn(self.n, self.m))]
-#     self.b_s += [nn.Parameter(self.b_std * t.randn(self.n))]
-#     import pdb
-#     pdb.set_trace()
-#     self.trl = TRL((self.d, self.n), 1, rank=self.n)
-
-#     # HT parameters
-#     if self.use_HT:
-#       self.HT_poolsize = HT_poolsize
-#       self.a_HT_std = .1
-#       a_scale = self.a_HT_std / np.sqrt(self.n)
-#       self.L_HT = int(np.ceil(np.log(self.d) / np.log(self.HT_poolsize)))
-#       self.a_HTs = t.nn.ParameterList()
-#       dim_l = self.d
-#       for _ in range(self.L_HT - 1):
-#         dim_l = int(np.ceil(dim_l / self.HT_poolsize))
-#         # at layer l we require dim_l * n * n parameters
-#         # self.a_HTs += [nn.Parameter(a_scale * t.randn(dim_l, self.n, self.n))]
-#         self.a_HTs += [
-#             nn.Parameter(self.n * t.eye(self.n).repeat(dim_l, 1, 1))
-#         ]
-#       self.a_HTs += [nn.Parameter(a_scale * t.randn(1, self.n, 1))]
-
-#   def phis(self, Xn, inds=...):
-#     # Xn is a M x 1 x n x dim(inds) tensor
-#     # returns M x n x dim(inds) tensor
-#     phis = Xn  # cleaner to move expand_X inside and take M x dim(inds) as input, like phidots
-
-#     # keep only the parameters relevant for the subset of variables specified by inds
-#     sliced_ws = [w[inds, ...] for w in self.w_s]
-#     sliced_bs = [b[inds, ...] for b in self.b_s]
-#     sliced_as = [a[inds, ...] for a in self.a_s]
-
-#     # compute CDF using a feed-forward network
-#     for w, b, a in zip(sliced_ws[:-1], sliced_bs[:-1], sliced_as[:-1]):
-#       phis = einsum('mjik,jikl->mjil', phis, self.nonneg_m(w)) + b
-#       phis = phis + t.tanh(phis) * t.tanh(a)
-
-#     phis = einsum('mjik,jikl->mjil', phis, self.nonneg_m(
-#         sliced_ws[-1])) + sliced_bs[-1]
-
-#     phis = self.phi(phis)
-#     return t.squeeze(phis, -1)
-
-#   def phidots(self, X, inds=...):
-#     # X is a M x dim(inds) tensor
-#     # returns M x n tensor
-
-#     phidots, fm = self.phidots_no_prod(X, inds=inds)
-#     phidots = t.prod(t.exp(fm).unsqueeze(1).unsqueeze(1) * phidots,
-#                      1)  # prod over d
-#     return t.squeeze(phidots), fm.squeeze()  # M x n
-
-#   def phidots_no_prod(self, X, inds=...):
-#     # X is a M x d tensor
-#     # returns M x d x n tensor
-
-#     phis = self.expand_X(X)  # m x d x n x 1
-#     phidots = t.ones_like(phis)
-#     for w, b, a in zip(self.w_s[:-1], self.b_s[:-1], self.a_s[:]):
-#       w = self.nonneg_m(w)
-#       a = t.tanh(a)
-#       phis = t.einsum('ijlk,lmk->ijlm', phis, w) + b
-#       phidots = t.einsum('ijlk,lmk->ijlm', phidots,
-#                          w) * (1 + a * utils.tanhdot(phis))
-#       phis = phis + a * t.tanh(phis)
-
-#     w = self.nonneg_m(self.w_s[-1])
-#     phis = t.einsum('ijlk,lk->ijl', phis, w) + self.b_s[-1]
-#     phidots = t.einsum('ijlk,lk->ijl', phidots, w) * utils.sigmoiddot(phis)
-#     # phis = utils.sigmoid(phis)
-
-#     fm = -t.log(phidots + 1e-10).detach()
-#     fm = fm.mean(1, True)  # mean over d
-#     fm = fm.min(2, True)[0]  # min over n
-
-#     return phidots, fm.squeeze()  # m x d x n x 1
-
-#   def expand_X(self, X):
-#     if len(X.shape) == 1:
-#       X = X.unsqueeze(-1)
-#     X = X.unsqueeze(-1).unsqueeze(-1)
-#     Xn = X.expand(-1, -1, self.n, 1)  # M x d x n x 1
-#     return Xn
-
-#   def likelihood(self, X, inds=...):
-#     # Evaluate joint likelihood at X
-#     # X : M x d tensor of sample points
-#     # inds : list of indices to restrict to (if interested in a subset of variables)
-
-#     if self.use_HT:
-#       phidots, fm = self.phidots_no_prod(X, inds)  # + 1e-10
-#       # phidots is M x d x n
-
-#       # regularize
-#       phidots = t.exp(fm).unsqueeze(1).unsqueeze(1) * phidots
-
-#       lk = self.HT_contraction(phidots, inds)
-#     else:
-#       phidots, fm = self.phidots_no_prod(X, inds)
-#       # regularize
-#       phidots = t.exp(fm).unsqueeze(1).unsqueeze(1) * phidots
-#       lk = self.trl(phidots)
-
-#     return lk, fm
-
-#   def log_density(self, X, inds=...):
-#     lk, fm = self.likelihood(X, inds)
-#     if inds is ...:
-#       n_vars = self.d
-#     else:
-#       n_vars = len(inds)
-#     return t.log(lk + 1e-10) - fm * n_vars
-
-#   def nll(self, X, inds=...):
-#     # negative log likelihood
-#     # X : M x d tensor of sample points
-#     return -t.mean(self.log_density(X, inds=inds))
 
 
 class CDFNet(nn.Module):
@@ -631,6 +424,138 @@ class CDFNet(nn.Module):
       return CCDF / denom
 
     return curr_condCDF
+
+
+class TenNetSingleBasis(nn.Module):
+  def __init__(
+      self,
+      d,
+      n=100,
+      L=4,
+      m=5,
+      w_std=0.1,
+      b_bias=0,
+      b_std=0,
+      a_std=1.0,
+      HT_poolsize=2,
+      use_HT=False,
+  ):
+    super(TenNet, self).__init__()
+    self.d = d
+    self.n = n
+    self.m = m
+    self.L = L
+    self.phi = t.sigmoid
+    self.phidot = lambda x: t.sigmoid(x) * (1 - t.sigmoid(x))
+    self.nonneg = t.nn.Softplus(10)
+    self.nonneg_m = t.nn.Softplus(10)  # try beta=1?
+    self.w_std = w_std
+    self.a_std = a_std
+    self.b_std = b_std
+    self.b_bias = b_bias
+    self.use_HT = use_HT
+
+    # initialize parameters for marginal CDFs
+    assert self.L >= 2
+    w_scale = self.w_std / t.sqrt(t.Tensor([self.m]))
+    self.w_s = t.nn.ParameterList(
+        [nn.Parameter(self.w_std * t.randn(self.n, self.m, 1))])
+    self.b_s = t.nn.ParameterList(
+        [nn.Parameter(self.b_std * t.randn(self.n, self.m))])
+    self.a_s = t.nn.ParameterList(
+        [nn.Parameter(self.a_std * t.randn(self.n, self.m))])
+    for i in range(self.L - 1):
+      self.w_s += [nn.Parameter(w_scale * t.randn(self.n, self.m, self.m))]
+      self.b_s += [nn.Parameter(self.b_std * t.randn(self.n, self.m))]
+      self.a_s += [nn.Parameter(self.a_std * t.randn(self.n, self.m))]
+
+    self.w_s += [nn.Parameter(w_scale * t.randn(self.n, self.m))]
+    self.b_s += [nn.Parameter(self.b_std * t.randn(self.n))]
+
+    if use_HT == True:
+      factorization = 'tucker'
+    else:
+      factorization = 'cp'
+
+    self.trl = TRL(input_shape=(self.d, self.n),
+                   output_shape=1,
+                   rank=self.n,
+                   factorization=factorization)
+    self.trl.init_from_random()
+
+  def phidots(self, X, inds=...):
+    # X is a M x dim(inds) tensor
+    # returns M x n tensor
+
+    phidots, fm = self.phidots_no_prod(X, inds=inds)
+    phidots = t.prod(t.exp(fm).unsqueeze(1).unsqueeze(1) * phidots,
+                     1)  # prod over d
+    return t.squeeze(phidots), fm.squeeze()  # M x n
+
+  def phidots_no_prod(self, X, inds=...):
+    # X is a M x d tensor
+    # returns M x d x n tensor
+
+    phis = self.expand_X(X)  # m x d x n x 1
+    phidots = t.ones_like(phis)
+    for w, b, a in zip(self.w_s[:-1], self.b_s[:-1], self.a_s[:]):
+      w = self.nonneg_m(w)
+      a = t.tanh(a)
+      phis = t.einsum('ijlk,lmk->ijlm', phis, w) + b
+      phidots = t.einsum('ijlk,lmk->ijlm', phidots,
+                         w) * (1 + a * utils.tanhdot(phis))
+      phis = phis + a * t.tanh(phis)
+
+    w = self.nonneg_m(self.w_s[-1])
+    phis = t.einsum('ijlk,lk->ijl', phis, w) + self.b_s[-1]
+    phidots = t.einsum('ijlk,lk->ijl', phidots, w) * utils.sigmoiddot(phis)
+    # phis = utils.sigmoid(phis)
+
+    fm = -t.log(phidots + 1e-10).detach()
+    fm = fm.mean(1, True)  # mean over d
+    fm = fm.min(2, True)[0]  # min over n
+
+    return phidots, fm.squeeze()  # m x d x n x 1
+
+  def expand_X(self, X):
+    if len(X.shape) == 1:
+      X = X.unsqueeze(-1)
+    X = X.unsqueeze(-1).unsqueeze(-1)
+    Xn = X.expand(-1, -1, self.n, 1)  # M x d x n x 1
+    return Xn
+
+  def likelihood(self, X, inds=...):
+    # Evaluate joint likelihood at X
+    # X : M x d tensor of sample points
+    # inds : list of indices to restrict to (if interested in a subset of variables)
+
+    phidots, fm = self.phidots_no_prod(X, inds)
+    # regularize
+    phidots = t.exp(fm).unsqueeze(1).unsqueeze(1) * phidots
+
+    # import pdb
+    # pdb.set_trace()
+
+    # normalize weights
+    weight = self.nonneg(self.trl.weight.to_tensor())
+    weight = weight / weight.sum(dim=1, keepdim=True)
+
+    lk = tenalg.inner(phidots, weight[inds, :, :], n_modes=2)
+
+    return lk, fm
+
+  def log_density(self, X, inds=...):
+    lk, fm = self.likelihood(X, inds)
+    if inds is ...:
+      n_vars = self.d
+    else:
+      n_vars = len(inds)
+    return t.log(lk + 1e-10) - fm * n_vars
+
+  def nll(self, X, inds=...):
+    # negative log likelihood
+    # X : M x d tensor of sample points
+    return -t.mean(self.log_density(X, inds=inds))
 
 
 class CopNet(nn.Module):
